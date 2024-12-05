84544df79fe4308ac1209348b302bebf50456c2b
Kevin Albarran <kalbarran@inland-engineering.com>
2024-12-05 00:20:18 -0800

    added react libaray since it sucked using vanilla javascript. Not that it was hard it was just tedious and i didnt want to have to write alot of html...

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>a asdfasdf!</h1>
+    </div>
+  );
+};
+
+export default App;
+
+
+// Render the App component into the DOM
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ Client/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.jsx', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, '../Server/wwwroot/js/reactapps'),
+    filename: 'main.js', // Output bundle
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Transpile .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/, // If you plan to use CSS
+        use: ['style-loader', 'css-loader'],
+      },
+      // Add other loaders (e.g., for images) if needed
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Resolve these extensions
+  },
+};
